7203efe7df067827d54a1c517d543420d51abd35
Henrique Salm Prim <henrique_salmprim@hotmail.com>
2023-09-04 22:00:41 -0300

    chore/build: implements ErrorHandler & uses cors

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
 import express from 'express';
+import cors from 'cors';
+import ErrorHandler from './Errors/ErrorHandler';
 
 const app = express();
 
 app.use(express.json());
+app.use(ErrorHandler.handle);
+app.use(cors());
 
 export default app;
